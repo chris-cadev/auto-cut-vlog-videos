c4a49bcd7bb49c11b55a17c9ea4b862ba9ed0d05
Christian Camacho <chris.ca.dev@gmail.com>
2024-01-21 19:58:17 -0800

    feat(audio) add audio filter to remove noise

--- autocut-videos.sh
+++ autocut-videos.sh
@@ -4,16 +4,17 @@ CWD=$(realpath $(dirname $0))
 
 mkdir -p "$CWD/${2:-"$1-autocutted"}"
 
-echo "---------------------- converting to mp4"
+echo "---------------------- cleaning audio"
 
 for video in ./$1/*.MOV; do
-  mp4_video=$( realpath "$CWD/$( dirname $video )/$( basename $video .MOV ).mp4" )
-  ffmpeg -y -i $( realpath "$CWD/$video" ) $mp4_video
+  clean_audio_video=$( realpath "$CWD/$( dirname $video )/$( basename $video .MOV )_clean-audio.MOV" )
+  # TODO: add audio filter to normalize levels
+  ffmpeg -y -i $( realpath "$CWD/$video" ) -af "afftdn=nr=10:nf=-30:tn=1" $clean_audio_video
 done
 
 echo "---------------------- cutting"
 
 for video in ./$1/*.MOV; do
-  mp4_video=$( realpath "$CWD/$( dirname $video )/$( basename $video .MOV ).mp4" )
-  poetry -C "$CWD" run unsilence -y $mp4_video "$CWD/${2:-"$1-autocutted"}/$(basename $video)"
+  clean_audio_video=$( realpath "$CWD/$( dirname $video )/$( basename $video .MOV )_clean-audio.MOV" )
+  poetry -C "$CWD" run unsilence -y $clean_audio_video "$CWD/${2:-"$1-autocutted"}/$(basename $video)"
 done
