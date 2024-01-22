af82d8e9349336c79c998cd15b23050c3363f006
Christian Camacho <chris.ca.dev@gmail.com>
2024-01-21 20:33:47 -0800

    fix(filenaming) change the way the files are generated

--- autocut-videos.sh
+++ autocut-videos.sh
@@ -6,15 +6,25 @@ mkdir -p "$CWD/${2:-"$1-autocutted"}"
 
 echo "---------------------- cleaning audio"
 
-for video in ./$1/*.MOV; do
-  clean_audio_video=$( realpath "$CWD/$( dirname $video )/$( basename $video .MOV )_clean-audio.MOV" )
-  # TODO: add audio filter to normalize levels
-  ffmpeg -y -i $( realpath "$CWD/$video" ) -af "afftdn=nr=10:nf=-30:tn=1" $clean_audio_video
+#!/bin/bash
+
+input_directory="./$1"
+
+for video in "$input_directory"/*.MOV; do
+  # Extract filename without extension
+  filename=$(basename "$video" .MOV)
+  
+  # Check if the filename does not end with "_clean-audio"
+  if [[ ! "$filename" =~ _clean-audio$ ]]; then
+    clean_audio_video=$(realpath "$CWD/$(dirname "$video")/${filename}_clean-audio.MOV")
+    # TODO: Add audio filter to normalize levels
+    ffmpeg -y -i "$(realpath "$CWD/$video")" -af "afftdn=nr=10:nf=-30:tn=1" "$clean_audio_video"
+  fi
 done
 
 echo "---------------------- cutting"
 
-for video in ./$1/*.MOV; do
-  clean_audio_video=$( realpath "$CWD/$( dirname $video )/$( basename $video .MOV )_clean-audio.MOV" )
+for video in "$input_directory"/*_clean-audio.MOV; do
+  clean_audio_video=$( realpath "$video" )
   poetry -C "$CWD" run unsilence -y $clean_audio_video "$CWD/${2:-"$1-autocutted"}/$(basename $video)"
 done
